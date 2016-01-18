9b36c1ff54dd1af3f772a2b9f4a11eb2f31bf518
Dan Moseley <danomoseley@gmail.com>
2016-01-18 11:04:07 -0500

    Backup config.py to remote server

--- backupDb.py
+++ backupDb.py
@@ -9,8 +9,12 @@ remote_path = os.path.join(config['remote']['path'], 'database')
 
 command = "rsync -a --bwlimit=500 %s/database/ %s@%s:%s" % (DIR, config['remote']['user'], \
     config['remote']['host'], remote_path)
-
 status, message = getstatusoutput(command)
+if status != 0:
+    print message
 
+command = "rsync -a --bwlimit=500 %s/config.py %s@%s:%s" % (DIR, config['remote']['user'], \
+    config['remote']['host'], config['remote']['path'])
+status, message = getstatusoutput(command)
 if status != 0:
     print message
