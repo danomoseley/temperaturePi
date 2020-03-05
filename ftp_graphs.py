3c4901712c41758aa877966eec1a0347e782fcc8
Dan Moseley <danomoseley@gmail.com>
2020-03-04 19:00:35 -0500

    Cache graphs for 5 minutes using expires header

--- ftp_graphs.py
+++ ftp_graphs.py
@@ -2,6 +2,7 @@
 import os
 from subprocess import getstatusoutput
 from config import config
+from datetime import datetime, timedelta
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -16,8 +17,10 @@ if os.path.isfile(os.path.join(DIR, 'database', 'lake_temp.rrd')):
 
 getstatusoutput(os.path.join(DIR, 'create_graphs.py'))
 
+expires = datetime.utcnow() + timedelta(minutes=5)
+
 s3_path = config['s3']
-command = "/usr/local/bin/aws s3 sync --quiet %s/latest_graphs/ %s/latest_graphs/ --cache-control max-age=0" % (DIR, s3_path)
+command = "/usr/local/bin/aws s3 sync --quiet %s/latest_graphs/ %s/latest_graphs/ --expires '%s'" % (DIR, s3_path, expires.isoformat())
 
 status, message = getstatusoutput(command)
 
